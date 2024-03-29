58c986a1e200621f65e6bfd507c619bffb30d00a
The real Nols <104772872+TheRealN0ls@users.noreply.github.com>
2024-03-29 16:24:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
 const links = [
-    { naam: "instagram", link: "https://www.instagram.com/the_real_nols/", color: "red" },
-    { naam: "reddit", link: "https://www.reddit.com/user/Dramgamermurf100000/", color: "orange"},
-    { naam: "github", link: "", color: "blue"}
+    { naam: "instagram", link: "https://www.instagram.com/the_real_nols/", color: "#ee2a7b", icon:"fa-brands fa-instagram" },
+    { naam: "steam", link: "https://steamcommunity.com/profiles/76561199086952972/", color: "#1b2838", icon:"fa-brands fa-steam" },
+    { naam: "reddit", link: "https://www.reddit.com/user/Dramgamermurf100000/", color: "#ff6314", icon:"fa-brands fa-reddit"},
+    { naam: "github", link: "https://github.com/TheRealN0ls/portfolio", color: "#0d74e7", icon:"fa-brands fa-github"}
 ];
 let html = "";
 links.forEach(element => {
     html +=
         `
         <div class="container-element">
+        <a target="_blank" href="${element.link}" style="background-color: ${element.color}"><i class="${element.icon}"></i></a>
         <h1>${element.naam}</h1>
-        <a target="_blank" href="${element.link}" style="background-color: ${element.color}">${element.naam}</a>
         </div>
         `;
 });
